30a3c2247ef3e1d93437b8081ad90098c3a6c87f
Mikolaj <abramowicz.mikolaj@gmail.com>
2025-12-01 15:21:35 +0100

    monitoring update

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -14,7 +14,12 @@ resource "aws_cloudwatch_dashboard" "apprunner" {
           stat    = "Maximum"
           period  = 60
           metrics = [
-            ["AWS/AppRunner", "ActiveInstances", "ServiceName", aws_apprunner_service.backend.service_name]
+            [
+              "AWS/AppRunner",
+              "ActiveInstances",
+              "ServiceName", aws_apprunner_service.backend.service_name,
+              "ServiceID",   aws_apprunner_service.backend.service_id
+            ]
           ]
         }
       },
@@ -24,12 +29,17 @@ resource "aws_cloudwatch_dashboard" "apprunner" {
         height = 6
         properties = {
           view    = "timeSeries"
-          title   = "App Runner Request Count"
+          title   = "App Runner Requests"
           region  = var.aws_region
           stat    = "Sum"
           period  = 60
           metrics = [
-            ["AWS/AppRunner", "RequestCount", "ServiceName", aws_apprunner_service.backend.service_name]
+            [
+              "AWS/AppRunner",
+              "Requests",
+              "ServiceName", aws_apprunner_service.backend.service_name,
+              "ServiceID",   aws_apprunner_service.backend.service_id
+            ]
           ]
         }
       }
